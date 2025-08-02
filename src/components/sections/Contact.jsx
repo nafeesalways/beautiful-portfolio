9b4875bd3bba3f5b92bfc6ec0a216dc245fe1ffb
nafeesalways <nafeezmy@gmail.com>
2025-08-02 16:01:41 +0600

    contact page updated

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReviewOnScroll from "../ReviewOnScroll";
+import emailjs from "emailjs-com";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+  
+    e.preventDefault();
+    emailjs
+      .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY)
+      .then((res) => {
+        console.log(res);
+        
+        setFormData({ name: "", email: "", message: "" });
+        toast.success("Message Sent!");
+      })
+      .catch(() => {
+        toast.error("Oops!!!Something Went wrong.Please Try Again.");
+      });
+  };
   return (
     <section
       id="contact"
@@ -12,7 +35,7 @@ const Contact = () => {
           <h2 className="text-3xl sm:text-4xl font-bold mb-10 text-center bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent">
             Get in Touch
           </h2>
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             {/* Name Field */}
             <div className="relative">
               <input
@@ -20,9 +43,14 @@ const Contact = () => {
                 id="name"
                 required
                 name="name"
+                value={formData.name}
                 placeholder="Your Name"
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white placeholder-white/60 transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
               />
+        
             </div>
 
             {/* Email Field */}
@@ -32,8 +60,12 @@ const Contact = () => {
                 id="email"
                 required
                 name="email"
+                value={formData.email}
                 placeholder="example@gmail.com"
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white placeholder-white/60 transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
               />
             </div>
 
@@ -43,9 +75,13 @@ const Contact = () => {
                 id="message"
                 required
                 name="message"
+                value={formData.message}
                 rows={5}
                 placeholder="Your Message..."
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white placeholder-white/60 transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
               />
             </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ToastContainer></ToastContainer>
    <div className='max-w-7xl mx-auto'>
     <App />
    </div>
